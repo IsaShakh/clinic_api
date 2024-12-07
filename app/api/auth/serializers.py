b590e81eda7939b6232f0f4b5c47e6abf89fcb61
IsaShakh <xxxisa2003@gmail.com>
2024-12-07 18:38:04 +0300

    serializer fix

--- app/api/auth/serializers.py
+++ app/api/auth/serializers.py
@@ -45,25 +45,20 @@ class UserUpdateSerializer(serializers.ModelSerializer):
     
     
 class PatientRegistrationSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  # Используем PrimaryKey для связи с пользователем
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  
 
     class Meta:
         model = Patient
         fields = ['user', 'phone', 'email']
 
     def create(self, validated_data):
-        # Получаем пользователя по ID из данных
         user = validated_data.pop('user')
-        
-        # Создаем пациента, привязывая его к существующему пользователю
         patient = Patient.objects.create(user=user, **validated_data)
         return patient
 
     def update(self, instance, validated_data):
-        # Получаем пользователя по ID
         user = validated_data.pop('user')
 
-        # Обновляем информацию о пользователе
         instance.user = user
         instance.phone = validated_data.get('phone', instance.phone)
         instance.email = validated_data.get('email', instance.email)
@@ -74,35 +69,26 @@ class PatientRegistrationSerializer(serializers.ModelSerializer):
     
     
 class DoctorRegistrationSerializer(serializers.ModelSerializer):
-    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  # Используем PrimaryKey для связи с пользователем
-    clinics = serializers.PrimaryKeyRelatedField(queryset=Clinic.objects.all(), many=True)  # Используем PrimaryKey для клиник
+    user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())  
+    clinics = serializers.PrimaryKeyRelatedField(queryset=Clinic.objects.all(), many=True)  
 
     class Meta:
         model = Doctor
         fields = ['user', 'specialization', 'clinics']
 
     def create(self, validated_data):
-        # Получаем пользователя и клиники по ID из данных
         user = validated_data.pop('user')
         clinics = validated_data.pop('clinics')
-        
-        # Создаем доктора
         doctor = Doctor.objects.create(user=user, **validated_data)
-
-        # Привязываем клиники к доктору
         doctor.clinics.set(clinics)
         return doctor
 
     def update(self, instance, validated_data):
-        # Получаем пользователя и клиники по ID
         user = validated_data.pop('user')
         clinics = validated_data.pop('clinics')
 
-        # Обновляем информацию о пользователе
         instance.user = user
         instance.specialization = validated_data.get('specialization', instance.specialization)
-
-        # Обновляем клиники
         instance.clinics.set(clinics)
         instance.save()
 
